2939a838472ce845f4abcb877d427142a7d98cea
Polygzz <139706982+Polygzz@users.noreply.github.com>
2025-04-17 22:02:50 -0700

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  // Add this line to enable environment variables in Astro
+  // This is usually not needed, but can be helpful in some cases
+  // vite: {
+  //   define: {
+  //     'process.env': process.env
+  //   }
+  // }
 });
